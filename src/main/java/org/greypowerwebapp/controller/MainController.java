f5c10ace403a53f1785fe0b4e9aee9d77d29262b
dcubas16 <dcubas16@gmail.com>
2014-06-24 09:45:20 -0500

    Creating new repository

---
+++ src/main/java/org/greypowerwebapp/controller/MainController.java
@@ -0,0 +1,59 @@
+package org.greypowerwebapp.controller;
+
+import java.util.Map;
+
+import org.greypowerwebapp.model.EmailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+	
+	@RequestMapping(value="home.htm", method=RequestMethod.GET )
+	public String home(Model model)
+	{
+		return "home";
+	}
+	
+	@RequestMapping(value="products.htm", method=RequestMethod.GET )
+	public String products(Model model)
+	{
+		return "products";
+	}
+	
+	@RequestMapping(value="aboutus.htm", method=RequestMethod.GET )
+	public String aboutus(Model model)
+	{
+		return "aboutus";
+	}
+	
+	@RequestMapping(value="productsList.htm", method=RequestMethod.GET )
+	public String productsList(Model model)
+	{
+		return "productsList";
+	}
+	
+	@RequestMapping(value="viewProducts.htm", method=RequestMethod.GET )
+	public String viewProducts(Model model)
+	{
+		return "viewProducts";
+	}
+	
+	@RequestMapping(value="contact.htm", method=RequestMethod.GET )
+	public String contact(Map<String, Object> model)
+	{
+		EmailMessage emailMessage = new EmailMessage();
+		model.put("emailSender", emailMessage);
+		
+		return "contact";
+	}
+	
+	@RequestMapping(value="services.htm", method=RequestMethod.GET )
+	public String services(Model model)
+	{
+		return "services";
+	}
+}
